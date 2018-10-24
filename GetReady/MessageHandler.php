1105869ca7f2b6e9209389e78bedf30900319aaa
DiablosWhisper <43128582+DiablosWhisper@users.noreply.github.com>
2018-10-24 15:24:31 +0300

    bv

--- GetReady/MessageHandler.php
+++ GetReady/MessageHandler.php
@@ -1,33 +1,29 @@
 <meta charset="utf-8"> 
 <?php
-error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
+error_reporting( E_ERROR );  
+ //Отключение предупреждений и нотайсов (warning и notice) на сайте
 // создание переменных из полей формы		
-if (isset($_POST['name1']))			{$name1			= $_POST['name1'];		if ($name1 == '')	{unset($name1);}}
-if (isset($_POST['email1']))		{$email1		= $_POST['email1'];		if ($email1 == '')	{unset($email1);}}
-if (isset($_POST['text']))			{$text			= $_POST['text'];		if ($text == '')	{unset($text);}}
-if (isset($_POST['sab']))			{$sab			= $_POST['sab'];		if ($sab == '')		{unset($sab);}}
+if (isset($_POST['Name']))		    {$name1		= $_POST['Name'];	    if ($Name == '')	{unset($Name);}}
+if (isset($_POST['Size']))		    {$email1	= $_POST['Size'];		if ($Size == '')	{unset($Size);}}
+if (isset($_POST['Submit']))		{$sab		= $_POST['Submit'];		if ($Submit == '')	{unset($Submit);}}
 //стирание треугольных скобок из полей формы
-if (isset($name1) ) {
-$name1=stripslashes($name1);
-$name1=htmlspecialchars($name1);
+if (isset($Name) ) {
+$Name=stripslashes($Name);
+$Name=htmlspecialchars($Name);
 }
-if (isset($email1) ) {
-$email1=stripslashes($email1);
-$email1=htmlspecialchars($email1);
-}
-if (isset($text) ) {
-$text=stripslashes($text);
-$text=htmlspecialchars($text);
+if (isset($Size) ) {
+$Size=stripslashes($Size);
+$Size=htmlspecialchars($Size);
 }
 // адрес почты куда придет письмо
 $address="getreadyshopua@gmail.com";
 // текст письма 
-$note_text="Тема : $urok \r\nИмя : $name1 \r\n Email : $email1 \r\n Дополнительная информация : $text";
+$note_text="Имя : $Name \r\n Размер : $Size \r\n ";
 
-if (isset($name1)  &&  isset ($sab) ) {
-mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
+if (isset($Name)  &&  isset ($Submit) ) {
+mail($address,$note_text,"Content-type:text/plain; windows-1251"); 
 // сообщение после отправки формы
-echo "<p style='color:#009900;'>Уважаемый(ая) <b>$name1</b> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b> $email1</b>.</p>";
+echo "<p style='color:#009900;'>Уважаемый(ая) <b>$Name</b> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b> $email1</b>.</p>";
 }
 
 ?>
