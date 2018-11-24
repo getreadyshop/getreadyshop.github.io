e93fe0a3c755e75f192e978bcded40f8f0f9a0cb
DiablosWhisper <43128582+DiablosWhisper@users.noreply.github.com>
2018-11-24 21:07:48 +0200

    er

--- GetReady/ProductPageScripts.js
+++ GetReady/ProductPageScripts.js
@@ -1,16 +1,22 @@
 function ChangeTShirt()
 {
     document.getElementById("Image").src="https://www.blackspin.gr/image/cache/catalog/product/STAR%20WARS,%20Galactic%20Empire,%20T-Shirt-600x600.png";
+
+    document.getElementById("Price").innerText="350 ₴";
 };
 
 function ChangeSweatShirt()
 {
     document.getElementById("Image").src="https://idolstore.net/wp-content/uploads/2017/07/m-rag-darthvader_1482275289_180-600x600.jpg";
+
+    document.getElementById("Price").innerText="570 ₴";
 };
 
-function ChangeHoody()
+function ChangeHoodie()
 {
     document.getElementById("Image").src="https://swoopstore.co.uk/wp-content/uploads/Star-Wars-Darth-Vader-Hoodie-02-600x600.jpg";
+
+    document.getElementById("Price").innerText="670 ₴";
 };
 
 function BuyProduct() 
